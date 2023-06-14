16b365036616227d2addce135f950436394855da
Nicole Martinez Perez <nickris1323@hotmail.com>
2023-06-14 11:11:38 -0400

    use keuboard input

--- src/CircleCompute.java
+++ src/CircleCompute.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class CircleCompute {
     public static void main (String[] args){
-        //   Scanner circle = new Scanner (System.in);
+         Scanner circle = new Scanner (System.in);
      //    System.out.println("radius");
-   //     double r = circle.nextDouble();
-        double r = 4;
-        double area = (Math.PI)*Math.pow(r,2);
-        double circumference = 2*r;
-        System.out.println("when the  radius is " + r + " the area is " + circumference + " ");
+       double radius = circle.nextDouble();
+        // double radius = 4.0;
+        double area = Math.PI*(radius*radius);
+        double circumference = 2*radius*Math.PI;
+        System.out.printf("when the  radius is %f the area is %f and the circumference is %f.",radius,area,circumference);
     }
 }
